340c6b9bafc0c9685a5ee3e58199cd7d0c026fa7
Ujjwal <ujjwal.jain116@gmail.com>
2024-10-08 00:54:25 +0530

    chmod

--- Frontend/src/client/Socket.jsx
+++ Frontend/src/client/Socket.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-render-hrm1.onrender.com");
 
 const Socket = () => {
   const [userID, setUserID] = useState(null);
